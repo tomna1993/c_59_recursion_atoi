f38fb1ec5068a9eca0154505047041de62607d2b
TomasNagy <tomi.nagy@outlook.com>
2023-06-07 16:51:55 +0200

    feat: check if argument passed

--- recursion_atoi.c
+++ recursion_atoi.c
@@ -3,5 +3,12 @@
 
 int main(int argc, string argv[])
 {
+    // Check if user inputs an argument
+    if (argc != 2)
+    {
+        printf ("Usage: ./recursion_atoi <positive integer>\n");
+        return 1;
+    }
+
     
 }
